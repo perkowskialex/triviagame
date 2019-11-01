6e42490fb9c6769aca83672b31d2894fb2a84bf8
Alex Perkowski <perkowskialex@gmail.com>
2019-11-01 09:54:39 -0500

    setting up psuedocode, add link to wireframe

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// https://wireframe.cc/HB9A1l
+
+// set constants
+    // trivia questions
+        // object 
+    // trivia answers split by question 
+
+// app state variables
+
+// functions
+        //initialize
+            // What type of trivia do you want to play --- splash
+            // Background music
+        //choose trivia 
+            // change background, change data
+                // randomize questions 
+                // randomize answers 
+        //choose answer
+            // array.some 
+            //if correct
+            //if wrong
+        //render
+            //whats the score
+            // whats the question and whats the question number
+            // is the game over?
+        //game over
+            // splash screen
+            // final score
+            // play again?  
+            //try a different trivia?
