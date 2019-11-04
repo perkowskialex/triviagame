b7a1f3cf8f3367129fac4807ce12390614ee08a5
Alex Perkowski <perkowskialex@gmail.com>
2019-11-04 13:34:42 -0600

    render function complete, score check works properly

--- js/main.js
+++ js/main.js
@@ -1,30 +1,25 @@
 // set constants
-    // trivia sets: array of objects of arrays? so...
 const presidentTrivia = [
     {
-    question: 'Who is the current president of the United States?',
-    answers: [
-        'Hillary Clinton','Barack Obama','George Washington', 'Donald Trump'
-    ],
-    correct: 'Donald Trump'
+        question: 'Who is the current president of the United States?',
+        answers: [
+            'Hillary Clinton','Barack Obama','George Washington', 'Donald Trump'
+        ],
+        correct: 'Donald Trump'
     },
     {
         question: 'Who was the first president of the United States?',
-        answers: {
-            a: 'Thomas Jefferson',
-            b: 'Alexander Hamilton',
-            c: 'John Adams'
-        },
-        correct: 'George Washington'
+        answers: [
+            'Thomas Jefferson', 'George Washington', 'Alexander Hamilton', 'John Adams'
+        ],
+        correct: 'George Washington',
     },
     {
-        question: 'XXXX?',
-        answers: {
-            a: 'X',
-            b: 'Y',
-            c: 'Z'
-        },
-        correct: '123'
+        question: 'Who was the only U. S. President to also serve as Chief Justice of the Supreme Court?',
+        answers: [
+            'William Howard Taft', 'Abraham Lincoln', 'Theodore Roosevelt', 'Millard Fillmore',
+        ],
+        correct: 'William Howard Taft'
     },  
 ];
 
@@ -56,6 +51,7 @@ function init() {
     console.log('running');
     triviaList = ['presidents','pizza','shrek','soccer'];
     console.log('trivia list: ' + triviaList);
+    answerList = [];
     }
             //console.log('running');
             // What type of trivia do you want to play --- splash
@@ -68,20 +64,67 @@ function init() {
 
 function triviaSelect() {
     if (event.target.innerHTML === 'U.S. Presidents Trivia') {
-        trivia.forEach(function(element, idx) {
-            element.textContent = presidentTrivia[0].answers[idx];
-            console.log(element.textContent);
-            idx++;
-        })
-        let questionNumber = 1;
-        console.log(questionNumber)
-        console.log('Running U.S. Presidents Trivia')
+        console.log('Running U.S. Presidents Trivia');
         body.style.backgroundColor = 'navy';
-        console.log('changing color to navy')
+        console.log('changing color to navy');
         $('.instructions').toggle();
-        welcome.textContent = presidentTrivia[0].question; //Question 1
+        presidents(); //Question 1
     }
+}
 
+function presidents() {
+    console.log('PRESIDENTS TRIVIA RUNNING')
+    let questionNumber = 1;
+    console.log('PRESIDENT TRIVIA QUESTION NUMBER = ' + questionNumber)
+    welcome.textContent = presidentTrivia[0].question;
+    trivia.forEach(function(element, idx) {
+        element.textContent = presidentTrivia[0].answers[idx]; //change button text content
+        element.style.backgroundColor = 'firebrick'
+        // console.log('Text content for ' + element + ' : ' + element.textContent); 
+        idx++; //iterate
+    });
+    // $('.play').addClass('.presidentAnswer').removeClass('.play');
+    $('.play').on(
+        'click', 
+        function(event) {
+            let text = event.target.textContent;
+            console.log(text);
+            answerList.push(text); //add to answer array
+            if (questionNumber === presidentTrivia.length) { //end function if out of questions
+                render();
+                return;
+                }
+            console.log(answerList);
+            welcome.textContent = presidentTrivia[questionNumber].question;
+            trivia.forEach(function(element,idx){
+                element.textContent = presidentTrivia[questionNumber].answers[idx];
+                console.log('Text content for ' + element + ' : ' + element.textContent); 
+                idx++; //iterate
+            })
+            questionNumber++
+            console.log("new question number is: "+ questionNumber);
+        }
+    )
+};
+
+function render() {
+    numberCorrect = 0;
+    console.log("RENDER FUNCTION");
+    console.log(answerList);  // ==> working now
+    presidentTrivia.forEach(function(element,idx) {
+        if (element.correct === answerList[idx]) {
+            numberCorrect++;
+        }
+        idx++
+    });
+    console.log('number correct: '+ numberCorrect);
+    numberWrong = answerList.length-numberCorrect; 
+    // check answerlist against correct answers
+    $('.play').toggle(); //remove buttonz
+    welcome.textContent = 'You scored ' + numberCorrect + " out of " + answerList.length;
+}
+
+// end of trivia select
             // change background, change data
             // pop out trivia from trivia array
         //choose answer
